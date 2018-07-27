d31ae32f2487debf72ded62113ee8cfadb093398
Duane Wessels <dwessels@verisign.com>
2018-07-27 23:01:39 +0000

    New measurement techniques and data

--- Time-Trials/hash-tree-depth-width/measure.sh
+++ Time-Trials/hash-tree-depth-width/measure.sh
@@ -3,23 +3,25 @@
 
 for D in `seq 0 9` ; do
     for B in `seq 1 26` ; do
-	SUM=0.0
-	for N in `seq 1 3`; do
-
+	BEST=99999999
+        for I in `seq 1 3`; do
 		#echo "Depth $D Branch $B #$N" 1>&2
 
 		/usr/bin/time --format '%U %S' --output /tmp/cpu \
-		../../ldns-zone-digest \
-			-v \
+		../../ldns-zone-digest-incremental \
+			-p 2 \
+			-c \
 			-D $D \
+			-W $B \
 			space \
 			space-20180725 \
-			2>/dev/null
-		CPU=`cat /tmp/cpu | sed -e's/ /+/' | bc -l`
-		SUM=`echo $SUM+$CPU | bc -l`
+			> /tmp/timing
+		T=`awk '/^TIMINGS:/ {print $5}' /tmp/timing`
+		if expr $T '<' $BEST >/dev/null ; then
+			BEST=$T
+		fi
 	done
-	CPU=`echo $SUM/$N | bc -l`
-	printf "%d %d %5.2f\n" $D $B $CPU
+	printf "%d %d %5.2f\n" $D $B $BEST
     done
     echo ""
 done
