f61a1cf4a9c3e5f42a332bace3b4e8085ebcc4ce
Duane Wessels <dwessels@verisign.com>
2018-07-31 23:33:20 +0000

    Reworking "hash-tree-depth-width" time trials and adding results from various TLD zones

--- Time-Trials/hash-tree-depth-width/measure.sh
+++ Time-Trials/hash-tree-depth-width/measure.sh
@@ -1,22 +1,31 @@
 #!/bin/sh
 #et -e
 
+ZONE=$1 ; shift
+YYYYMMDD=$1 ; shift
+
+if test "$ZONE" = "root" ; then
+	OWNER="."
+else
+	OWNER=$ZONE
+fi
+
 for D in `seq 0 9` ; do
     for B in `seq 1 26` ; do
 	BEST=99999999
         for I in `seq 1 3`; do
 		#echo "Depth $D Branch $B #$N" 1>&2
 
-		/usr/bin/time --format '%U %S' --output /tmp/cpu \
 		../../ldns-zone-digest-incremental \
+			-t \
 			-p 2 \
 			-c \
 			-D $D \
 			-W $B \
-			space \
-			space-20180725 \
-			> /tmp/timing
-		T=`awk '/^TIMINGS:/ {print $5}' /tmp/timing`
+			$OWNER \
+			../../Zones/$ZONE-$YYYYMMDD \
+			> /tmp/timing.$$
+		T=`awk '/^TIMINGS:/ {print $5}' /tmp/timing.$$`
 		if expr $T '<' $BEST >/dev/null ; then
 			BEST=$T
 		fi
