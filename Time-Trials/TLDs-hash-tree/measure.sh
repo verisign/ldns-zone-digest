d91703ceb849b23d09511fcdcf2d2ff496d51286
Duane Wessels <dwessels@verisign.com>
2018-08-10 14:28:26 +0000

    perf tests: measure all TLDs with "incremental" version

---
+++ Time-Trials/TLDs-hash-tree/measure.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+set -e
+
+MODE=$1 ; shift
+ALG=$1 ; shift
+YYYYMMDD=$1 ; shift
+
+UDIR=Undigested-$YYYYMMDD
+DDIR=Digested-$YYYYMMDD-alg$ALG
+mkdir -p $UDIR
+mkdir -p $DDIR
+
+LOG=/dev/null
+if test "$MODE" = "-c" ; then
+	LOG=calculate-alg$ALG.log
+elif test "$MODE" = "-v" ; then
+	LOG=validate-alg$ALG.log
+fi
+
+cp /dev/null ${LOG}_
+
+for ZF in /zfa/Zones/*/2018/*-${YYYYMMDD}.gz ; do
+	ZN=`basename $ZF | sed -e "s/-${YYYYMMDD}.gz//"`
+	echo "$ZN..."
+	test "$ZN" = "com" && continue
+	if test -s $UDIR/$ZN ; then
+		echo "$UDIR/$ZN already exists"
+	else
+		gzip -dc $ZF > $UDIR/$ZN
+	fi
+	NR=`wc -l < $UDIR/$ZN`
+	NRSET=`cat $UDIR/$ZN | awk '{print $1,$4}' | sort | uniq -i | wc -l`
+	if test "$MODE" = "-c" ; then
+		sort --random-sort $UDIR/$ZN \
+		| ../../ldns-zone-digest-incremental \
+			-t \
+			-p $ALG \
+			-D 5 -W 13 \
+			-c \
+			-o $DDIR/$ZN \
+			$ZN \
+			$UDIR/$ZN \
+		> /tmp/timing.$$
+		T=`awk '/^TIMINGS:/ {print $5}' /tmp/timing.$$`
+	elif test "$MODE" = "-v" ; then
+		sort --random-sort $DDIR/$ZN \
+		| ../../ldns-zone-digest-incremental \
+			-t \
+			-v \
+			-D 5 -W 13 \
+			$ZN \
+		> /tmp/timing.$$
+		T=`awk '/^TIMINGS:/ {print $7}' /tmp/timing.$$`
+	fi
+	printf "%s %d %d %5.2f\n" $ZN $NR $NRSET $T >> ${LOG}_
+done
+
+sort -n -k 2 < ${LOG}_ > ${LOG}
+rm -f ${LOG}_
+
+rm -v /tmp/timing.$$
